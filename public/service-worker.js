06b42987dab5ff215b744682b495396099508d1f
Reiner Bonsol <reinerbonsol93@gmail.com>
2020-08-12 23:13:14 -0400

    had some syntax error in servicerworker that needed to be fixed

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,6 +1,3 @@
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
-
 const FILES_TO_CACHE = [
   '/styles.css',
   '/index.js',
@@ -9,20 +6,22 @@ const FILES_TO_CACHE = [
   '/icons/icon-512x512.png'
 ];
 
-// install
-self.addEventListener("install", function(evt) {
-  evt.waitUntil(
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
+
+self.addEventListener("install", event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       console.log("Your files were pre-cached successfully!");
       return cache.addAll(FILES_TO_CACHE);
     })
   );
-
   self.skipWaiting();
 });
 
-self.addEventListener("activate", function(evt) {
-  evt.waitUntil(
+self.addEventListener("activate", event => {
+  event.waitUntil(
     caches.keys().then(keyList => {
       return Promise.all(
         keyList.map(key => {
@@ -34,40 +33,32 @@ self.addEventListener("activate", function(evt) {
       );
     })
   );
-
   self.clients.claim();
 });
 
 // fetch
-self.addEventListener("fetch", function(evt) {
-  // cache successful requests to the API
-  if (evt.request.url.includes("/api/")) {
-    evt.respondWith(
+self.addEventListener("fetch", event => {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
-        return fetch(evt.request)
+        return fetch(event.request)
           .then(response => {
-            // If the response was good, clone it and store it in the cache.
             if (response.status === 200) {
-              cache.put(evt.request.url, response.clone());
+              cache.put(event.request.url, response.clone());
             }
-
             return response;
           })
           .catch(err => {
-            // Network request failed, try to get it from the cache.
-            return cache.match(evt.request);
+            return cache.match(event.request);
           });
       }).catch(err => console.log(err))
     );
-
     return;
   }
 
-  // if the request is not for the API, serve static assets using "offline-first" approach.
-  // see https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook#cache-falling-back-to-network
-  evt.respondWith(
-    caches.match(evt.request).then(function(response) {
-      return response || fetch(evt.request);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
     })
   );
 });
